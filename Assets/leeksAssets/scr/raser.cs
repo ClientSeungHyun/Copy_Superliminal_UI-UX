ab83c6f625742b398ce5782e75d10a9188dc164d
rudtnsiasia <leeks1106@naver.com>
2025-04-16 18:23:56 +0900

    04.16
    linerander,raycast final

--- Assets/leeksAssets/scr/raser.cs
+++ Assets/leeksAssets/scr/raser.cs
@@ -1,62 +1,61 @@
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading;
-using TMPro;
-using Unity.VisualScripting;
-using Unity.XR.CoreUtils;
+using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.Assertions;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
 public class raser : MonoBehaviour
 {
     public LineRenderer laserLine; // 레이저 시각화용
     public float laserDistance = 50f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Transform trackingSpace;
-    public GameObject LeftController;
-
+    public GameObject RController;
 
     void Start()
     {
-        //UnityEngine.Debug.Log("잘됨");
         laserLine.positionCount = 2;
         laserLine.useWorldSpace = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         FireLaser();
-
     }
 
     void FireLaser()
     {
-        Vector3 localPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
-        Quaternion localRot = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        // LeftController의 위치와 회전 정보 가져오기
+        Vector3 localPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        Quaternion localRot = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
 
-        // CameraRig을 기준으로 좌표 변환
+        // TrackingSpace 기준으로 월드 좌표로 변환
         Vector3 worldPos = trackingSpace.TransformPoint(localPos);
         Vector3 forward = trackingSpace.TransformDirection(localRot * Vector3.forward);
         forward.Normalize();
 
-        // 3. 레이저 시각화
-        laserLine.enabled = true;
-        laserLine.SetPosition(0, LeftController.transform.position);
+        // 레이저 시작 위치를 LeftController의 위치로 설정
+        laserLine.SetPosition(0, RController.transform.position);
 
+        // 레이저 발사용 Ray 설정
         Ray ray = new Ray(LeftController.transform.position, forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, laserDistance))
         {
+            // 레이저가 충돌한 지점까지의 위치를 설정
             laserLine.SetPosition(1, hit.point);
+
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            {
+                UnityEngine.Debug.Log("오른쪽 검지 버튼 눌림!");
+                // 충돌한 게임 오브젝트 가져오기
+                GameObject hitObject = hit.collider.gameObject;
+                UnityEngine.Debug.Log("Hit object: " + hitObject.name);
+
+            }
+
         }
         else
         {
+            // 충돌하지 않은 경우 레이저가 최대 거리까지 계속 가도록 설정
             laserLine.SetPosition(1, LeftController.transform.position + forward * laserDistance);
         }
     }
-
 }
